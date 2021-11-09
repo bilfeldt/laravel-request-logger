8e3b2dc47688d8da4898886d60c9cddd92351d60
Anders Bilfeldt <abilfeldt@gmail.com>
2021-11-09 14:35:54 +0100

    Update migration and factory

--- database/factories/RequestLogFactory.php
+++ database/factories/RequestLogFactory.php
@@ -12,7 +12,34 @@ class RequestLogFactory extends Factory
     public function definition()
     {
         return [
+            'uuid' => $this->faker->uuid,
+            'ip' => $this->faker->ipv4,
+            'session' => null,
+            'status' => $this->faker->randomElement([200, 201, 202, 204, 300, 301, 302, 303, 304, 400, 401, 402, 403, 404, 405, 406, 422, 429, 500, 501, 502, 503, 504]),
+            'method' => $this->faker->randomElements(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
+            'route' => $this->faker->domainWord().'.'.$this->faker->randomElements(['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']),
+            'path' => null,
+            'headers' => $this->getHeaders(),
+            'payload' => $this->getPayload(),
+            'response_headers' => $this->getHeaders(),
+            'response_body' => $this->faker->sentence(),
+            'time' => $this->faker->numberBetween(100, 30000),
+            'memory' => $this->faker->randomFloat(1, 1, 100),
+        ];
+    }
 
+    protected function getPayload(): array
+    {
+        return [
+            'type' => $this->faker->mimeType(),
+            'extension' => $this->faker->fileExtension(),
+        ];
+    }
+
+    protected function getHeaders(): array
+    {
+        return [
+            'User-Agent' => $this->faker->userAgent(),
         ];
     }
 }
