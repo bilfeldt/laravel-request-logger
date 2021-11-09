43308ffca943e22f3e208a04d975ecf0d5389b13
Anders Bilfeldt <abilfeldt@gmail.com>
2021-11-09 14:57:21 +0100

    Add logging of middleware

--- database/factories/RequestLogFactory.php
+++ database/factories/RequestLogFactory.php
@@ -15,6 +15,7 @@ class RequestLogFactory extends Factory
             'uuid' => $this->faker->uuid,
             'ip' => $this->faker->ipv4,
             'session' => null,
+            'middleware' => $this->faker->words(3),
             'status' => $this->faker->randomElement([200, 201, 202, 204, 300, 301, 302, 303, 304, 400, 401, 402, 403, 404, 405, 406, 422, 429, 500, 501, 502, 503, 504]),
             'method' => $this->faker->randomElements(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
             'route' => $this->faker->domainWord().'.'.$this->faker->randomElements(['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']),
